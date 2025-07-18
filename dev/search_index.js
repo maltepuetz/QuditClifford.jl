f0e3d6e31309e0579e61008d2cc8b5e8b0bb8f82
Documenter.jl <documenter@juliadocs.github.io>
2025-07-18 13:44:49 +0000

    build based on 24f3a78

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#QuditClifford","page":"Home","title":"QuditClifford","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuditClifford.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
